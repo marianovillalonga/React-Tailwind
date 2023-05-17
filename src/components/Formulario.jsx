e1e8c6d95af8d38c85417a9765f37c1023b243b5
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2023-05-16 23:27:46 -0300

    Avances

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,11 @@
 const Formulario = () => {
     return (
-        <div>
-            <h1>Formulario</h1>
+        <div className="w-1/2 lg:w-2/5">
+            <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+            <p className="text-lg mt-5 text-center">
+                Añade pacientes y {''}
+                <span className="text-indigo-600 font-bold">Administralos</span>
+            </p>
         </div>
     )
 }
