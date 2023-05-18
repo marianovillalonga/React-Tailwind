14ed47cdafae8542d5549c6324303ab1d11f91a0
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2023-05-17 23:37:26 -0300

    Creo const para facilitar codigo

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,25 +1,28 @@
 const Pacientes = ({paciente}) => {
+
+    const {nombre, propietario, email, alta, sintomas} = paciente
+
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold mb-3 text-gray-700 uppercase">
                 Nombre: {''}
-                <span className="font-normal normal-case">{paciente.nombre}</span>
+                <span className="font-normal normal-case">{nombre}</span>
             </p>
             <p className="font-bold mb-3 text-gray-700 uppercase">
                 Propietario: {''}
-                <span className="font-normal normal-case">{paciente.propietario}</span>
+                <span className="font-normal normal-case">{propietario}</span>
             </p>
             <p className="font-bold mb-3 text-gray-700 uppercase">
                 Email: {''}
-                <span className="font-normal normal-case">{paciente.email}</span>
+                <span className="font-normal normal-case">{email}</span>
             </p>
             <p className="font-bold mb-3 text-gray-700 uppercase">
                 Alta: {''}
-                <span className="font-normal normal-case">{paciente.alta}</span>
+                <span className="font-normal normal-case">{alta}</span>
             </p>
             <p className="font-bold mb-3 text-gray-700 uppercase">
                 Sintomas: {''}
-                <span className="font-normal normal-case">{paciente.sintomas}</span>
+                <span className="font-normal normal-case">{sintomas}</span>
             </p>
         </div>
     )
