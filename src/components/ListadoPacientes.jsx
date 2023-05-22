29e8b9df34a77061a44d45045c00dbdcea73a971
marianovillalonga <78375048+marianovillalonga@users.noreply.github.com>
2023-05-22 19:14:54 -0300

    Eliminar paciente

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Pacientes from "./Pacientes"
 
-const ListadoPacientes = ({pacientes, setPaciente}) => {
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
             {
@@ -16,6 +16,7 @@ const ListadoPacientes = ({pacientes, setPaciente}) => {
                                 key={paciente.id}
                                 paciente={paciente}
                                 setPaciente={setPaciente}
+                                eliminarPaciente={eliminarPaciente}
                             />
                         ))}
                     </>
--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,7 +1,15 @@
-const Pacientes = ({paciente, setPaciente}) => {
+const Pacientes = ({paciente, setPaciente, eliminarPaciente}) => {
 
-    const {nombre, propietario, email, alta, sintomas} = paciente
+    const {nombre, propietario, email, alta, sintomas, id} = paciente
 
+    const handleEliminar = () => {
+        const respuesta = confirm('Desea eliminar este paciente?')
+
+        if(respuesta) {
+            eliminarPaciente(id)
+        }
+    }
+    
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold mb-3 text-gray-700 uppercase">
@@ -38,6 +46,7 @@ const Pacientes = ({paciente, setPaciente}) => {
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700
                     text-white font-bold uppercase rounded-lg"
+                    onClick={handleEliminar}
                 >
                     Eliminar
                 </button>
